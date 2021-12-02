99eea5ff21b923e460e3f49ac63f57e80cb368dd
natearogers1 <natearogers1@gmail.com>
2021-12-01 22:44:40 -0800

    day 1 and 2

---
+++ src/day1.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+use std::{thread::sleep, time::Duration};
+
+pub fn run() {
+    let file = match File::open("inputs/day1.txt") {
+        Err(why) => panic!("couldn't open: {}", why),
+        Ok(file) => file,
+    };
+
+    let reader = BufReader::new(&file);
+
+    let lines: Vec<i32> = reader
+        .lines()
+        .map(|x| x.unwrap().parse::<i32>())
+        .collect::<Result<_, _>>()
+        .unwrap();
+    // why can't I do this
+
+    let part1_result = part1(&lines);
+    print!("part1 result: {}\n", part1_result);
+
+    let part2_result = part2(&lines);
+    print!("part2 result: {}\n", part2_result);
+}
+
+fn part1(lines: &Vec<i32>) -> i32 {
+    let mut num_larger: i32 = 0;
+
+    for window in lines.windows(2) {
+        if window[1] > window[0] {
+            num_larger += 1;
+            //print!("num_larger - {}\n\n", num_larger);
+            //sleep(Duration::from_millis(10000));
+        }
+    }
+    return num_larger;
+}
+
+fn part2(lines: &Vec<i32>) -> i32 {
+    let mut num_larger: i32 = 0;
+
+    for big_window in lines.windows(4) {
+        let sum_window1: &i32 = &big_window[..3].iter().sum();
+        let sum_window2: &i32 = &big_window[1..].iter().sum();
+        if sum_window2 > sum_window1 {
+            num_larger += 1
+            //sleep(Duration::from_millis(2000));
+        }
+    }
+    return num_larger;
+}
---
+++ src/day2.rs
@@ -0,0 +1,100 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::path::Path;
+use std::str::FromStr;
+
+pub fn run() {
+    // read the inputs from file
+    let movement_strings = read_file("inputs/day2.txt");
+
+    // instantiate new struct position with 0 values
+    let mut position = Position { x: 0, y: 0, aim: 0 };
+
+    // cast the input lines into a single vector consisting of Movement structs Arr
+    let movements: Vec<Movement> = movement_strings.iter().map(|l| new_movement(l)).collect();
+
+    part1(&movements);
+    part2(&movements);
+}
+
+fn part1(movements: &Vec<Movement>) {
+    // instantiate new struct position with 0 values
+    let mut position = Position { x: 0, y: 0, aim: 0 };
+    // for each movement increment the appropriate member of struct Position
+    for movement in movements {
+        match movement.direction {
+            Direction::Up => position.y -= movement.value,
+            Direction::Down => position.y += movement.value,
+            Direction::Forward => position.x += movement.value,
+        }
+    }
+    println!("{:?}", position);
+    println!("product: {}", position.x * position.y);
+}
+
+fn part2(movements: &Vec<Movement>) {
+    // instantiate new struct position with 0 values
+    let mut position = Position { x: 0, y: 0, aim: 0 };
+
+    for movement in movements {
+        match movement.direction {
+            Direction::Up => position.aim -= movement.value,
+            Direction::Down => position.aim += movement.value,
+            Direction::Forward => {
+                position.x += movement.value;
+                position.y += position.aim * movement.value;
+            }
+        }
+    }
+    println!("{:?}", position);
+    println!("product: {}", position.x * position.y);
+}
+
+enum Direction {
+    Up,
+    Down,
+    Forward,
+}
+impl FromStr for Direction {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Direction, Self::Err> {
+        match input {
+            "up" => Ok(Direction::Up),
+            "down" => Ok(Direction::Down),
+            "forward" => Ok(Direction::Forward),
+            _ => Err(()),
+        }
+    }
+}
+struct Movement {
+    direction: Direction,
+    value: u32,
+}
+
+#[derive(Debug)]
+struct Position {
+    x: u32,
+    y: u32,
+    aim: u32,
+}
+
+fn new_movement(movement: &str) -> Movement {
+    let m: Vec<&str> = movement.split(' ').collect();
+    let direction = m[0];
+    let direction_enum = Direction::from_str(direction).unwrap();
+    let value = m[1].parse::<u32>().unwrap();
+
+    return Movement {
+        direction: direction_enum,
+        value: value,
+    };
+}
+
+fn read_file(file: &str) -> Vec<String> {
+    let reader = BufReader::new(File::open(file).expect("could not open file"));
+    reader
+        .lines()
+        .map(|l| l.expect("could not parse line"))
+        .collect()
+}
